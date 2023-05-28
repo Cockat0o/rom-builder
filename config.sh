574ee5d6c1aa302bbd75071d56633b159dc2a77e
Cockat0o <134852548+Cockat0o@users.noreply.github.com>
2023-05-28 20:36:40 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=Cockat0o
+GITHUB_EMAIL=yahazemlak@gmail.com
 
-KBUILD_BUILD_USER="Aki"
+KBUILD_BUILD_USER="Cockat0o"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-export oem=motorola
-export device=harpia
+export oem=samsung
+export device=a73xq
 
 ROM="PixelExperience"
 manifest_url="https://github.com/PixelExperience/manifest"
 export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+branch="thirteen"
 
 release_repo="Akianonymus/harpia_builds"
 
